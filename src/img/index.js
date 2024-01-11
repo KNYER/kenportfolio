757c15c5d75ff0f6db9c9fe5968282777c61a7d0
KNRE <20-06399@g.batstate-u.edu.ph>
2024-01-11 22:50:54 +0800

    Style: icon changed

--- src/img/index.js
+++ src/img/index.js
@@ -4,7 +4,6 @@ const Images ={
     resume: require('./MARTINEZ_RESUME.pdf'),
     secqr: require('./secqr.jpg'),
     flomo: require('./flomo.jpg'),
-    video: require('./video.mp4'),
     imgKenR: require('./kenR.jpg'),
     weather_app: require('./weather_app.jpg'),
     ushoot: require('./ushoot.jpg'),
