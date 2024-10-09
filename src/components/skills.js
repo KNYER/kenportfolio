035b6d58a26cdf41111ad1d743c2ded3d18ff93b
KNRE <20-06399@g.batstate-u.edu.ph>
2024-10-10 02:32:47 +0800

    skills:hover/layout

--- src/components/skills.js
+++ src/components/skills.js
@@ -11,17 +11,19 @@ const SKILLS = () => {
     });
 
     return ( 
+        
         <div className="skills">
             <h1>{text} <Cursor/></h1>
         <div className="skcontainer">
             <div className="skicons">
-            <p><FaHtml5 /></p>
-            <p><FaCss3 /></p>
-            <p><FaJs /></p>
-            <p><FaPhp /></p>
-            <p><FaPython /></p>
-            <p><FaReact /></p>
-            <p><FaJava /></p>
+            <p><FaHtml5 /><span className="sktitle"><h2>HTML5</h2></span></p>
+            <p><FaCss3 /><span className="sktitle"><h2>CSS3</h2></span></p>
+            <p><FaJs /><span className="sktitle"><h2>JavaScript</h2></span></p>
+            <p><FaReact /><span className="sktitle"><h2>React.js</h2></span></p>
+            <p><FaPhp /><span className="sktitle"><h2>PHP</h2></span></p>
+            <p><FaPython /><span className="sktitle"><h2>Python</h2></span></p>
+            <p><DiMysql /><span className="sktitle"><h2>MySQL</h2></span></p>
+            <p><FaJava /><span className="sktitle"><h2>Java</h2></span></p>
             </div>
         </div>
         </div>
